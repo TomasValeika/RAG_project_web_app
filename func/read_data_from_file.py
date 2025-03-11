b6227b40651d063ad94ca5f7b6b7719a27baa436
TomasValeika <117848092+TomasValeika@users.noreply.github.com>
2025-03-11 12:50:36 +0200

    Add files via upload

--- func/read_data_from_file.py
+++ func/read_data_from_file.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from docx import Document
+import glob
+import os
 
 
 def read_csv(temp_file, uploaded_file, file_name):
@@ -64,3 +66,49 @@ def read_word(temp_file, uploaded_file, file_name):
     df_id_overview = df[["id_overview"]]
 
     return df_id_overview
+
+
+def get_recomendation_id(answer) -> pd.DataFrame:
+    """
+    Get answer and return filtered results by id
+    """
+    temp_folder = r"data/temp_files/"
+    lst_file = glob.glob(os.path.join(temp_folder, "*"))
+    temp_file = []
+    for i in lst_file:
+        if i.endswith("txt"):
+            continue
+        else:
+            temp_file.append(i.split("\\")[-1])
+
+    id_lst = []
+    if temp_file[0].endswith(".csv"):
+        df = pd.read_csv(f"data/temp_files/{temp_file[0]}")
+        for id in range(0, len(answer)):
+            id_lst.append(int(answer[id].page_content.split(" ")[0].strip('"')))
+
+        df = df.loc[df["id"].isin(id_lst)]
+
+        return df
+
+    elif temp_file[0].endswith(".xlsx"):
+        df = pd.read_excel(f"data/temp_files/{temp_file[0]}")
+        for id in range(0, len(answer)):
+            id_lst.append(int(answer[id].page_content.split(" ")[0].strip('"')))
+
+        df = df.loc[df["id"].isin(id_lst)]
+
+        return df
+
+    else:
+        document = Document(f"data/temp_files/{temp_file[0]}")
+        table = document.tables[0]
+        df = pd.DataFrame([[cell.text for cell in row.cells] for row in table.rows])
+        df = df.rename(columns=df.iloc[0]).drop(df.index[0]).reset_index(drop=True)
+        for id in range(0, len(answer)):
+            id_lst.append(int(answer[id].page_content.split(" ")[0].strip('"')))
+        df["id"] = df["id"].astype("int")
+
+        df = df.loc[df["id"].isin(id_lst)]
+
+        return df
